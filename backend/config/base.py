59bacbfb0bcb554a08f4a2019650a10ee56396c4
tembokbaleko123 <poloking52980@gmail.com>
2026-07-31 19:06:54 +0800

    fix: media structure access

--- backend/config/base.py
+++ backend/config/base.py
@@ -95,7 +95,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (uploaded images)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'backend' / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
